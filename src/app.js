f4764b607de40c97c2c82bb00d70669492181357
AndyGH86 <141663667+AndyGH86@users.noreply.github.com>
2023-10-10 23:27:06 +0200

    generando domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    function generateDomainNames(pronoun, adj, noun) {
+      const domainNames = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+      for (let p of pronoun) {
+        for (let a of adj) {
+          for (let n of noun) {
+            const domainName = `${p}${a}${n}.com`;
+            domainNames.push(domainName);
+          }
+        }
+      }
+
+      return domainNames;
+    }
+
+    // Función para mostrar los resultados en la página web
+    function displayResults() {
+      const domainNames = generateDomainNames(pronoun, adj, noun);
+      const resultsList = document.getElementById('results');
+
+      // Limpiar la lista de resultados
+      resultsList.innerHTML = '';
+
+      // Agregar cada nombre de dominio a la lista
+      domainNames.forEach((name) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = name;
+        resultsList.appendChild(listItem);
+      });
+    }
+
+    // Agregar un evento click al botón para generar los nombres de dominio
+    const generateButton = document.getElementById('generateButton');
+    generateButton.addEventListener('click', displayResults);
